001029594c91a80afc9732e65e1c2e88c451d58d
Evan <evan.ucd@gmail.com>
2024-03-02 21:45:13 +0000

    evan-fix the map size background color

--- DublinBikes/flask_app/static/index.js
+++ DublinBikes/flask_app/static/index.js
@@ -1,36 +1,43 @@
-// Ensure initMap() is called after the page loads
+// Ensure initMap() is called after the page has loaded
 function initMap() {
-  var dublin = { lat: 53.349805, lng: -6.26031 };
+  var dublin = { lat: 53.349805, lng: -6.26031 }; // Coordinates for Dublin
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 14,
-    center: dublin,
+    zoom: 14, // Initial zoom level
+    center: dublin, // Center the map on Dublin
   });
 
   var marker = new google.maps.Marker({
-    position: dublin,
+    position: dublin, // Position the marker at Dublin's coordinates
     map: map,
-    title: "Dublin",
+    title: "Dublin", // Title that appears when hovering over the marker
   });
 }
 
-// Assuming you have a button for displaying the map
+// Assuming you have a button to display the map
 document.querySelector(".clickMap").addEventListener("click", function () {
   var mapContainer = document.getElementById("mapContainer");
   var chartsDropdown = document.querySelector(".charts-dropdown");
 
+  // Display the map container
   mapContainer.style.display = "block";
   mapContainer.style.visibility = "visible";
-  chartsDropdown.style.display = "block"; // Display the charts dropdown menu
-  initMap(); // Initialize the map
+  // Display the charts dropdown menu
+  chartsDropdown.style.display = "block";
+  // Initialize the map
+  initMap();
 });
 
+// Event listener for when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
   var toggleBtn = document.getElementById('toggleNav');
   var navbar = document.querySelector('.navbar');
   var mapContainer = document.getElementById('mapContainer');
 
+  // Add a click event listener to the toggle button
   toggleBtn.addEventListener('click', function() {
+    // Toggle the 'hidden' class on the navbar
     navbar.classList.toggle('hidden');
+    // Toggle the 'fullwidth' class on the map container
     mapContainer.classList.toggle('fullwidth');
   });
 });
