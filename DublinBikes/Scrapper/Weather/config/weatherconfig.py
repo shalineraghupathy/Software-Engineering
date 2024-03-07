b8782255fc07a919683afbe23b466375a47b428d
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-03-07 13:27:24 +0000

    apikey updated

--- DublinBikes/Scrapper/Weather/config/weatherconfig.py
+++ DublinBikes/Scrapper/Weather/config/weatherconfig.py
@@ -1,4 +1,6 @@
-apiKey="f47ac38f6822ee7534f4cef684309e37"
+apiKey="e5a7467f3b567b2e260e9bc3d40dd2b0"
+# "f47ac38f6822ee7534f4cef684309e37"
+# e5a7467f3b567b2e260e9bc3d40dd2b0
 apiURI= "https://api.openweathermap.org/data/2.5/weather"
 city="dublin"
 countrycode="IE"
