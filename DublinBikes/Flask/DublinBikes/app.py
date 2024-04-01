1b899bb95502e9935bf55e8bb652dee1aa5ed49c
Shaline Raghupathy <shalineraghupathy02@gmail.com>
2024-04-01 05:16:47 +0100

    fix issues with data fetching

--- DublinBikes/Flask/DublinBikes/app.py
+++ DublinBikes/Flask/DublinBikes/app.py
@@ -6,62 +6,37 @@ import data
 
 app = Flask(__name__)
 
-stations = data.fetch_bikes_data()
-
-
 @app.route('/')
 def index():
+    return render_template("index.html")
 
-    try:
-
-        # Set up the markers
-        markers = []
-        for station in stations:
-            latitude = station.get('position', {}).get('lat', 0.0)
-            longitude = station.get('position', {}).get('lng', 0.0)
-
-            marker = {
-                'number': station['number'],
-                'position': {'lat': latitude,'lng':longitude},
-                'title': station['name'],
-                'status': station['status'],
-                'bike_stands': station['bike_stands'],
-                'available_bikes': station['available_bikes'],
-            }
-
-            markers.append(marker)
-        # print(markers)
-
-        # Render the template with API key, markers, and specified lat and lng
-        return render_template("index.html",data=markers)
-    except:
-        print(traceback.format_exc())
-        return "error in index", 404  
     
 @app.route('/stations')
 def get_stations():
-        print("In get Stations")
-        markers = []
-        for station in stations:
-            latitude = station.get('position', {}).get('lat', 0.0)
-            longitude = station.get('position', {}).get('lng', 0.0)
+        try:
+            stations = data.fetch_bikes_data()
+            markers = []
+            for station in stations:
+                latitude = station.get('position', {}).get('lat', 0.0)
+                longitude = station.get('position', {}).get('lng', 0.0)
 
-            marker = {
-                'number': station['number'],
-                'lng':longitude,
-                'lat':latitude,
-                'position': {'lat': latitude,'lng':longitude},
-                'title': station['name'],
-                'status': station['status'],
-                'bike_stands': station['bike_stands'],
-                'available_bikes': station['available_bikes'],
-                'available_bike_stands': station['available_bike_stands']
-            }
+                marker = {
+                    'number': station['number'],
+                    'lng':longitude,
+                    'lat':latitude,
+                    'position': {'lat': latitude,'lng':longitude},
+                    'title': station['name'],
+                    'status': station['status'],
+                    'bike_stands': station['bike_stands'],
+                    'available_bikes': station['available_bikes'],
+                    'available_bike_stands': station['available_bike_stands']
+                }
 
-            markers.append(marker)
-        # print(markers)
-        return {'stations': markers}
-
-
+                markers.append(marker)
+            # print(markers)
+            return {'stations': markers}
+        except:
+            print(traceback.format_exc())
+            return "error in index", 404  
 if __name__ == '__main__':
     app.run(debug=True)
