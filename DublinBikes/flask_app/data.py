9a4f8cb802f541c6a5f2751244a42303bd57300d
Evan <evan.ucd@gmail.com>
2024-04-11 15:55:51 +0100

    other files

---
+++ DublinBikes/flask_app/data.py
@@ -0,0 +1,151 @@
+import requests
+import config
+import db
+import time
+import datetime
+import threading
+
+
+def formatdate(value):
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(value))
+
+
+def fetch_bikes_data():
+    params = {'apiKey': config.apiKey_b, 'contract': config.contract}
+    response = requests.get(config.apiURI_b, params=params)
+    if response.status_code == 200:
+        stations = response.json()
+        return stations
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+
+def fetch_weather_data():
+    params = {'q': f'{config.city},{config.countrycode}', 'appid': config.apiKey_w}
+    response = requests.get(config.apiURI_w, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+
+def scrape_bikes_data():
+    while True:
+        stations = fetch_bikes_data()
+
+        for station in stations:
+            latitude = station.get('position', {}).get('lat', 0.0)
+            longitude = station.get('position', {}).get('lng', 0.0)
+
+            # Create a dictionary with the station data
+            station_data = {
+                'number': station['number'],
+                'contract_name': station['contract_name'],
+                'name': station['name'],
+                'address': station['address'],
+                'position_lat': latitude,
+                'position_long': longitude,
+                'banking': station['banking'],
+                'bonus': station['bonus'],
+                'bike_stands': station['bike_stands']
+            }
+
+            update_datetime = formatdate(station['last_update'] // 1000)
+
+            # Create a dictionary with the availability data
+            availability_data = {
+                'number': station['number'],
+                'available_bikes': station['available_bikes'],
+                'available_bike_stands': station['available_bike_stands'],
+                'last_update': update_datetime,
+                'status': station['status']
+            }
+
+            db.add_or_update_station_data(station_data)
+            db.add_availability_data(availability_data)
+        print("Records pushed to db", datetime.datetime.now())
+        print("Next load starts in 5 minutes")
+        time.sleep(1 * 60)
+
+
+def scrape_weather_data():
+    while True:
+        data = fetch_weather_data()
+        # Check if 'weather' is a list and get the first element
+        weather_info = data.get('weather', [{}])[0]
+
+        position_lat = data.get('coord', {}).get('lat', 0.0)
+        position_long = data.get('coord', {}).get('lon', 0.0)
+        weather_id = weather_info.get('id', 0.0)
+        weather_main = weather_info.get('main', "")
+        weather_description = weather_info.get('description', "")
+        weather_icon = weather_info.get('icon', 0.0)
+
+        temperature = data.get('main', {}).get('temp', 0.0)
+        feels_like = data.get('main', {}).get('feels_like', 0.0)
+        temp_min = data.get('main', {}).get('temp_min', 0.0)
+        temp_max = data.get('main', {}).get('temp_max', 0.0)
+        pressure = data.get('main', {}).get('pressure', 0.0)
+        humidity = data.get('main', {}).get('humidity', 0.0)
+        visibility = data.get('visibility', 0.0)
+        wind_speed = data.get('wind', {}).get('speed', 0.0)
+        wind_deg = data.get('wind', {}).get('deg', 0.0)
+        clouds_all = data.get('clouds', {}).get('all', 0.0)
+        last_update = formatdate(data.get('dt', 0))
+        sys_type = data.get('sys', {}).get('type', 0.0)
+        sys_id = data.get('sys', {}).get('id', 0.0)
+        sys_country = data.get('sys', {}).get('country', "")
+        sys_sunrise = formatdate(data.get('sys', {}).get('sunrise', 0))
+        sys_sunset = formatdate(data.get('sys', {}).get('sunset', 0))
+        timezone = data.get('timezone', 0)
+        city_id = data.get('id', 0)
+        city_name = data.get('name', "")
+        cod = data.get('cod', 0)
+
+        # Create a dictionary with the weather data
+        weather_data = {
+            'weather_main': weather_main,
+            'weather_description': weather_description,
+            'weather_id': weather_id,
+            'weather_icon': weather_icon,
+            'position_lat': position_lat,
+            'position_long': position_long,
+            'temperature': temperature,
+            'feels_like': feels_like,
+            'temp_min': temp_min,
+            'temp_max': temp_max,
+            'pressure': pressure,
+            'humidity': humidity,
+            'visibility': visibility,
+            'wind_speed': wind_speed,
+            'wind_deg': wind_deg,
+            'clouds_all': clouds_all,
+            'last_update': last_update,
+            'sys_type': sys_type,
+            'sys_id': sys_id,
+            'sys_country': sys_country,
+            'sys_sunrise': sys_sunrise,
+            'sys_sunset': sys_sunset,
+            'timezone': timezone,
+            'id': city_id,
+            'name': city_name,
+            'cod': cod
+        }
+
+        db.add_weather_data(weather_data)
+        time.sleep(1 * 60)
+
+
+def main():
+    thread1 = threading.Thread(target=scrape_bikes_data)
+    thread1.start()
+    thread2 = threading.Thread(target=scrape_weather_data)
+    thread2.start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ DublinBikes/flask_app/db.py
@@ -0,0 +1,225 @@
+from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+import config
+from sqlalchemy.engine.url import URL
+from sqlalchemy import inspect
+from sqlalchemy.exc import SQLAlchemyError
+
+Base = declarative_base()
+
+
+class Station(Base):
+    __tablename__ = 'station'
+
+    number = Column(Integer, primary_key=True)
+    contract_name = Column(String(256))
+    name = Column(String(256))
+    address = Column(String(256))
+    position_lat = Column(Float)
+    position_long = Column(Float)
+    banking = Column(Boolean)
+    bonus = Column(Boolean)
+    bike_stands = Column(Integer)
+
+    availability = relationship('Availability', back_populates='station')
+
+    def __init__(self, number, contract_name, name, address, position_lat, position_long, banking, bonus, bike_stands):
+        self.number = number
+        self.contract_name = contract_name
+        self.name = name
+        self.address = address
+        self.position_lat = position_lat
+        self.position_long = position_long
+        self.banking = banking
+        self.bonus = bonus
+        self.bike_stands = bike_stands
+
+    def __str__(self):
+        return (f"number = {self.number}, contract_name = {self.contract_name}, name = {self.name}, "
+                f"address= {self.address}, position_lat= {self.position_lat}, position_long= {self.position_long}, "
+                f"banking= {self.banking}, bonus= {self.bonus}, bike_stands= {self.bike_stands}")
+
+
+class Availability(Base):
+    __tablename__ = 'availability'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    number = Column(Integer, ForeignKey('station.number', ondelete='CASCADE'))
+    available_bikes = Column(Integer)
+    available_bike_stands = Column(Integer)
+    last_update = Column(DateTime)
+    status = Column(String(256))
+
+    station = relationship('Station', back_populates='availability')
+
+    def __init__(self, number, available_bikes, available_bike_stands, last_update, status):
+        self.number = number
+        self.available_bikes = available_bikes
+        self.available_bike_stands = available_bike_stands
+        self.last_update = last_update
+        self.status = status
+
+    def __str__(self):
+        return (f"number= {self.number},  available_bikes= {self.available_bikes}, "
+                f"available_bike_stands= {self.available_bike_stands}, last_update= {self.last_update}, status= {self.status}")
+
+
+class Weather(Base):
+    __tablename__ = 'weather'
+
+    number = Column(Integer, primary_key=True, autoincrement=True)
+    weather_main = Column(String(256))
+    weather_description = Column(String(256))
+    weather_id = Column(Integer)
+    weather_icon = Column(String(256))
+    position_lat = Column(Float)
+    position_long = Column(Float)
+    temperature = Column(Float)
+    feels_like = Column(Float)
+    temp_min = Column(Float)
+    temp_max = Column(Float)
+    pressure = Column(Float)
+    humidity = Column(Float)
+    visibility = Column(Float)
+    wind_speed = Column(Float)
+    wind_deg = Column(Float)
+    clouds_all = Column(Float)
+    last_update = Column(DateTime)
+    sys_type = Column(Float)
+    sys_id = Column(Integer)
+    sys_country = Column(String(256))
+    sys_sunrise = Column(DateTime)
+    sys_sunset = Column(DateTime)
+    timezone = Column(Float)
+    id = Column(Integer)
+    name = Column(String(256))
+    cod = Column(Integer)
+
+    def __init__(self, weather_main, weather_description, weather_id, weather_icon, position_lat,
+                 position_long, temperature, feels_like, temp_min, temp_max, pressure, humidity,
+                 visibility, wind_speed, wind_deg, clouds_all, last_update, sys_type, sys_id, sys_country,
+                 sys_sunrise, sys_sunset, timezone, id, name, cod):
+        self.weather_main = weather_main
+        self.weather_description = weather_description
+        self.weather_id = weather_id
+        self.weather_icon = weather_icon
+        self.position_lat = position_lat
+        self.position_long = position_long
+        self.temperature = temperature
+        self.feels_like = feels_like
+        self.temp_min = temp_min
+        self.temp_max = temp_max
+        self.pressure = pressure
+        self.humidity = humidity
+        self.visibility = visibility
+        self.wind_speed = wind_speed
+        self.wind_deg = wind_deg
+        self.clouds_all = clouds_all
+        self.last_update = last_update
+        self.sys_type = sys_type
+        self.sys_id = sys_id
+        self.sys_country = sys_country
+        self.sys_sunrise = sys_sunrise
+        self.sys_sunset = sys_sunset
+        self.timezone = timezone
+        self.id = id
+        self.name = name
+        self.cod = cod
+    # Create a SQLite database engine
+
+
+def db_engine():
+    try:
+        #     db_url = URL.create(
+        #     drivername='mysql+pymysql',
+        #     username=config.username,
+        #     password=config.password,
+        #     host=config.dbEndpoint,
+        #     port=config.port,
+        #     database=config.dbName
+        # )
+        db_url = URL.create(
+            drivername='mysql+pymysql',
+            username="root",
+            password="Shachu@0203",
+            host="localhost",
+            port="3306",
+            database="dbikestest"
+        )
+        engine = create_engine(db_url, echo=False)
+        return engine
+    except Exception as ex:
+        print("An exception occured: ", ex)
+        return None
+
+
+engine = db_engine()
+
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+
+
+def create_tables():
+    try:
+        with Session() as session:
+            inspector = inspect(engine)
+            if not inspector.has_table('weather'):
+                Base.metadata.create_all(engine)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+create_tables()
+
+
+# Function to add or update a station
+def add_or_update_station_data(data):
+    try:
+        with Session() as session:
+            existing_station = session.query(Station).filter_by(number=data['number']).first()
+
+            # if existing_station:
+            #     # Update existing station
+            #     for key, value in data.items():
+            #         setattr(existing_station, key, value)
+
+            if existing_station:
+                # Update existing station
+                keys_to_update = ['contract_name', 'name', 'address', 'position_lat', 'position_long', 'banking',
+                                  'bonus', 'bike_stands']
+                for key in keys_to_update:
+                    setattr(existing_station, key, data[key])
+            else:
+                # Add new station
+                station = Station(**data)
+                session.add(station)
+                # print("Added new station - ", station)
+            session.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+# Function to add availability data
+def add_availability_data(data):
+    try:
+        with Session() as session:
+            availability = Availability(**data)
+            session.add(availability)
+            session.commit()
+            # print("Added new availability - ", availability)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+# Function to add weather data
+def add_weather_data(data):
+    try:
+        with Session() as session:
+            weather = Weather(**data)
+            session.add(weather)
+            session.commit()
+    except SQLAlchemyError as e:
+        print(f"Error adding weather data to the database: {e}")
